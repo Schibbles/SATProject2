08489b63ab9a9e0803af7cb682a723c9bf43ca8e
David Schejbal <dwschejbal@outlook.com>
2021-08-05 15:04:13 -0500

    Initial Commit

---
+++ SATProject2/SATProject2.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SATProject2.UI.MVC.MvcApplication" Language="C#" %>
---
+++ SATProject2/SATProject2.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATProject2.UI.MVC.Startup))]
+namespace SATProject2.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
