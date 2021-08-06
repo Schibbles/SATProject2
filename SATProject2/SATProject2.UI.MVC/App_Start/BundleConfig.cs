a5b0c1b34c13f59557bb3292c5ba380b839c9f55
David Schejbal <dwschejbal@outlook.com>
2021-08-06 11:20:26 -0500

    Home buttons added

--- SATProject2/SATProject2.UI.MVC/App_Start/BundleConfig.cs
+++ SATProject2/SATProject2.UI.MVC/App_Start/BundleConfig.cs
@@ -23,8 +23,11 @@ namespace SATProject2.UI.MVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      //"~/Content/bootstrap.css",
+                      //"~/Content/site.css",
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/font-awesome.min.css",
+                      "~/Content/css/templatemo_style.css"));
         }
     }
 }
